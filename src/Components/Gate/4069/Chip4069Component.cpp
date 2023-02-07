db0b40837d74a317947ed97543be48984f2077fc
Thyodas <guillaume.hein@epitech.eu>
2023-02-07 23:19:33 +0100

    feat: add chip 4069

---
+++ src/Components/Gate/4069/Chip4069Component.cpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** NanoTekSpice
+** File description:
+** Chip4069Component.cpp
+*/
+
+#include "Chip4069Component.hpp"
+
+nts::Tristate nts::Chip4069Component::compute(std::size_t pin)
+{
+    return getLink(pin);
+}
+
+nts::Chip4069Component::Chip4069Component()
+{
+    _validPins = {
+        Input1, Output1,
+        Input2, Output2,
+        Input3, Output3,
+        Input4, Output4,
+        Input5, Output5,
+        Input6, Output6,
+    };
+
+    notGate1.setLink(NotComponent::Input, *this, Input1);
+    this->setLink(Output1, notGate1, NotComponent::Output);
+
+    notGate2.setLink(NotComponent::Input, *this, Input2);
+    this->setLink(Output2, notGate2, NotComponent::Output);
+
+    notGate3.setLink(NotComponent::Input, *this, Input3);
+    this->setLink(Output3, notGate3, NotComponent::Output);
+
+    notGate4.setLink(NotComponent::Input, *this, Input4);
+    this->setLink(Output4, notGate4, NotComponent::Output);
+
+    notGate5.setLink(NotComponent::Input, *this, Input5);
+    this->setLink(Output5, notGate5, NotComponent::Output);
+
+    notGate6.setLink(NotComponent::Input, *this, Input6);
+    this->setLink(Output6, notGate6, NotComponent::Output);
+}
---
+++ src/Components/Gate/4069/Chip4069Component.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2023
+** NanoTekSpice
+** File description:
+** Chip4069Component.hpp
+*/
+
+#pragma once
+
+#include "../../AComponent.hpp"
+#include "../../Elementary/Not/NotComponent.hpp"
+
+namespace nts {
+    class Chip4069Component : public AComponent {
+        public:
+            enum Pin {
+                Input1 = 1, Output1 = 2,
+                Input2 = 3, Output2 = 4,
+                Input3 = 5, Output3 = 6,
+                Input4 = 9, Output4 = 8,
+                Input5 = 11, Output5 = 10,
+                Input6 = 13, Output6 = 12,
+            };
+
+            Chip4069Component();
+
+            Tristate compute(std::size_t pin) override;
+        private:
+            NotComponent notGate1;
+            NotComponent notGate2;
+            NotComponent notGate3;
+            NotComponent notGate4;
+            NotComponent notGate5;
+            NotComponent notGate6;
+    };
+} // nts
