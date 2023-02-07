fd39dc8d570c56918e3196e28a30f8d2c29ea21e
Thyodas <guillaume.hein@epitech.eu>
2023-02-07 22:37:46 +0100

    feat: add chip 4081 (and)

---
+++ src/Components/Gate/4081/Chip4081Component.cpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2023
+** NanoTekSpice
+** File description:
+** Chip4081Component.cpp
+*/
+
+#include "Chip4081Component.hpp"
+
+nts::Tristate nts::Chip4081Component::compute(std::size_t pin)
+{
+    return getLink(pin);
+}
+
+nts::Chip4081Component::Chip4081Component()
+{
+    _validPins = {
+        Input1A, Input1B, Output1Y,
+        Input2A, Input2B, Output2Y,
+        Input3A, Input3B, Output3Y,
+        Input4A, Input4B, Output4Y,
+    };
+
+    andGate1.setLink(AndComponent::InputA, *this, Input1A);
+    andGate1.setLink(AndComponent::InputB, *this, Input1B);
+    this->setLink(Output1Y, andGate1, AndComponent::Output);
+
+    andGate2.setLink(AndComponent::InputA, *this, Input2A);
+    andGate2.setLink(AndComponent::InputB, *this, Input2B);
+    this->setLink(Output2Y, andGate2, AndComponent::Output);
+
+    andGate3.setLink(AndComponent::InputA, *this, Input3A);
+    andGate3.setLink(AndComponent::InputB, *this, Input3B);
+    this->setLink(Output3Y, andGate3, AndComponent::Output);
+
+    andGate4.setLink(AndComponent::InputA, *this, Input4A);
+    andGate4.setLink(AndComponent::InputB, *this, Input4B);
+    this->setLink(Output4Y, andGate4, AndComponent::Output);
+}
---
+++ src/Components/Gate/4081/Chip4081Component.hpp
@@ -0,0 +1,43 @@
+/*
+** EPITECH PROJECT, 2023
+** NanoTekSpice
+** File description:
+** Chip4081Component.hpp
+*/
+
+#pragma once
+
+#include "../../AComponent.hpp"
+#include "../../Elementary/And/AndComponent.hpp"
+
+namespace nts {
+    class Chip4081Component : public AComponent {
+        public:
+            enum Pin {
+                Input1A = 1,
+                Input1B = 2,
+                Output1Y = 3,
+
+                Input2A = 5,
+                Input2B = 6,
+                Output2Y = 4,
+
+                Input3A = 8,
+                Input3B = 9,
+                Output3Y = 10,
+
+                Input4A = 12,
+                Input4B = 13,
+                Output4Y = 11,
+            };
+
+            Chip4081Component();
+
+            Tristate compute(std::size_t pin) override;
+        private:
+            AndComponent andGate1;
+            AndComponent andGate2;
+            AndComponent andGate3;
+            AndComponent andGate4;
+    };
+} // nts
